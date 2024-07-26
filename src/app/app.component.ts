674a4403e1fcd481f17dd55fc61fb254a0a07dd5
dostonabdusattorov <webdeveloperdoston@gmail.com>
2024-07-26 23:11:23 +0500

    configuration, button component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from './shared/ui/button/button.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'republic_toys';
---
+++ src/app/shared/ui/link-button/link-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-link-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './link-button.component.html',
+  styleUrls: ['./link-button.component.scss']
+})
+export class LinkButtonComponent {
+
+}
