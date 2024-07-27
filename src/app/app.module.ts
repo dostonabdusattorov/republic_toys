f7c8537982cf28cee7315b1fba55c0632bebb5c3
dostonabdusattorov <webdeveloperdoston@gmail.com>
2024-07-27 16:00:00 +0500

    button, footer and header components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './shared/ui/button/button.component';
+import { ButtonComponent, HeaderComponent, FooterComponent } from './shared';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  exports: [ButtonComponent, HeaderComponent, FooterComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/ui/index.ts
@@ -0,0 +1,4 @@
+export * from './button/button.component';
+export * from './footer/footer.component';
+export * from './header/header.component';
+export * from './link-button/link-button.component';
