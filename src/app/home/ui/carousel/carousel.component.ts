e6126fe9fec695dd270e891ac98d9cb196e1b4ed
dostonabdusattorov <webdeveloperdoston@gmail.com>
2024-08-06 02:43:44 +0500

    home hero slider fixed

--- src/app/home/ui/carousel/carousel.component.ts
+++ src/app/home/ui/carousel/carousel.component.ts
@@ -6,8 +6,22 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent {
+  currentSlide: number = 0;
+  next() {
+    if (this.currentSlide < 3) {
+      this.currentSlide++;
+    }
+  }
+  prev() {
+    if (this.currentSlide > 0) {
+      this.currentSlide--;
+    }
+  }
 
+  get transformStyle(): string {
+    return `translateX(calc(107rem * ${-this.currentSlide}))`;
+  }
 }
